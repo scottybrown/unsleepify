799c1776da6ac782ae4ecf7bb93d71546d887acd
Scott Brown <scottyb.3378@gmail.com>
2018-04-05 19:36:46 +1000

    initial

---
+++ app/src/main/java/com/naur/unsleepify/MainActivity.java
@@ -0,0 +1,13 @@
+package com.naur.unsleepify;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
