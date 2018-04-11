3d470794fd20b1e134306c0d4e3cfc6c3c80c73d
Scott Brown <scottyb.3378@gmail.com>
2018-04-11 22:14:27 +1000

    shows existing alarm, populates to 8h past right now, needs cleaning

--- app/src/main/java/com/naur/unsleepify/MainActivity.java
+++ app/src/main/java/com/naur/unsleepify/MainActivity.java
@@ -5,15 +5,20 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.time.LocalDate;
 import java.util.Calendar;
+import java.util.Date;
 
-// doesn't like having more than 1 alarm. seems to overwrite.
+// show when the one alarm is set
 // should store a list of alarms rather than just sending broadcast receivers out into the ether. then show that list.
 // shuffle songs playing
 //√ todo get playlist, play songs
@@ -30,51 +35,88 @@ import java.util.Calendar;
 // make time default to 8h from now
 // report how far away you set an alarm
 public class MainActivity extends Activity {
+    public static final String SAVED_ALARM_IN_MILLIS="SAVED_ALARM_IN_MILLIS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final NumberPicker hourPicker=findViewById(R.id.HourPicker);
+        final NumberPicker hourPicker = findViewById(R.id.HourPicker);
         hourPicker.setMinValue(1);
         hourPicker.setMaxValue(24);
 
-        final NumberPicker minutePicker=findViewById(R.id.MinutePicker);
+        final NumberPicker minutePicker = findViewById(R.id.MinutePicker);
         minutePicker.setMinValue(1);
         minutePicker.setMaxValue(60);
 
-        Button submitButton=findViewById(R.id.SubmitButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        TextView existingAlarm = findViewById(R.id.ExistingAlarm);
+        long defaultValue=-1l;
+        long existingAlarmInMillis=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getLong(SAVED_ALARM_IN_MILLIS, defaultValue);
+        if(existingAlarmInMillis!=-1l) {
+            Calendar existingAlarmCalendar=Calendar.getInstance();
+            existingAlarmCalendar.setTimeInMillis(existingAlarmInMillis);
+            long existingAlarmHour = existingAlarmCalendar.get(Calendar.HOUR_OF_DAY);
+            long existingAlarmMinute = existingAlarmCalendar.get(Calendar.MINUTE);
+            existingAlarm.setText(existingAlarmHour + ":" + existingAlarmMinute);
+//            hourPicker.setValue((int)existingAlarmHour);
+//            minutePicker.setValue((int)existingAlarmMinute);
+//        }else{
+        }
+        Calendar cal=Calendar.getInstance();
+            Calendar defaultTime=calculateAlarmTime(cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
+            hourPicker.setValue(defaultTime.get(Calendar.HOUR_OF_DAY));
+            minutePicker.setValue(defaultTime.get(Calendar.MINUTE));
 
+
+        Button submitButton = findViewById(R.id.SubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+// todo clean up all cal stuff
+            // should always default to current date.
             @Override
             public void onClick(View v) {
-                setupRepeatingBroadcastReceiver(getTimeInMillis(hourPicker.getValue(),minutePicker.getValue()));
-                toastify("Alarm configured for "+hourPicker.getValue()+":"+minutePicker.getValue());
+                Calendar cal=Calendar.getInstance();
+                cal.set(Calendar.HOUR_OF_DAY,hourPicker.getValue());
+                cal.set(Calendar.MINUTE, minutePicker.getValue());
+                setupRepeatingBroadcastReceiver(cal);
+
+                SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
+                editor.putLong(SAVED_ALARM_IN_MILLIS,cal.getTimeInMillis());
+                editor.apply();
             }
         });
 
     }
 
-    public long getTimeInMillis(long hour, long minute) {
-        Calendar alarmStartTime = Calendar.getInstance();
+    public static Calendar getEightHoursFromNow(long hour, long minutes) {
+        long eightHoursFromNow = hour + 8;
         Calendar now = Calendar.getInstance();
-        alarmStartTime.set(Calendar.HOUR_OF_DAY, (int)hour);
-        alarmStartTime.set(Calendar.MINUTE, (int)minute);
-        alarmStartTime.set(Calendar.SECOND, 0);
 
-        if (now.after(alarmStartTime)) {
-            alarmStartTime.add(Calendar.DATE, 1);
+        if (eightHoursFromNow > 23) {
+            eightHoursFromNow = eightHoursFromNow - 24;
+            now.add(Calendar.DAY_OF_MONTH, 1);
         }
-
-        return alarmStartTime.getTimeInMillis();
+        now.set(Calendar.HOUR_OF_DAY, (int) eightHoursFromNow);
+        return now;
     }
 
-    public void setupRepeatingBroadcastReceiver(long alarmTimeInMillis) {
+    public static Calendar calculateAlarmTime(long hour, long minute) {
+        Calendar time = getEightHoursFromNow(hour, minute);
+        time.set(Calendar.SECOND, 0);
+
+        if (Calendar.getInstance().after(time)) {
+            time.add(Calendar.DATE, 1);
+        }
+
+        return time;
+    }
+
+    public void setupRepeatingBroadcastReceiver(Calendar alarmTime) {
         AlarmManager alarmManager = (AlarmManager) this.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this.getApplicationContext(), AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 0, intent, 0);
 
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, alarmTimeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, alarmTime.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
     }
 
     private void toastify(String text) {
--- app/src/test/java/com/naur/unsleepify/ExampleUnitTest.java
+++ app/src/test/java/com/naur/unsleepify/ExampleUnitTest.java
@@ -2,16 +2,30 @@ package com.naur.unsleepify;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void canSkipAheadEightHours() {
+        assertCalendarIsTime(MainActivity.getEightHoursFromNow(0,0), 8,true);
+        assertCalendarIsTime(MainActivity.getEightHoursFromNow(15,0), 23,true);
+        assertCalendarIsTime(MainActivity.getEightHoursFromNow(16,0), 0,false);
+        assertCalendarIsTime(MainActivity.getEightHoursFromNow(17,0), 1,false);
+        assertCalendarIsTime(MainActivity.getEightHoursFromNow(20,0), 4,false);
+
+    }
+
+    private void assertCalendarIsTime(Calendar calendar, int hour, boolean isToday) {
+        assertEquals(calendar.get(Calendar.HOUR_OF_DAY),hour);
+        assertEquals(calendar.get(Calendar.MINUTE),Calendar.getInstance().get(Calendar.MINUTE));
+        assertEquals(calendar.get(Calendar.SECOND),Calendar.getInstance().get(Calendar.SECOND));
+
+        Calendar day = Calendar.getInstance();
+        if(!isToday){day.add(Calendar.DAY_OF_MONTH,1);}
+        assertEquals(calendar.get(Calendar.DAY_OF_MONTH),day.get(Calendar.DAY_OF_MONTH));
+        assertEquals(calendar.get(Calendar.YEAR),day.get(Calendar.YEAR));
+        assertEquals(calendar.get(Calendar.MONTH),day.get(Calendar.MONTH));
     }
 }
