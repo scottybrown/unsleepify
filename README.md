6eeb9db745fa7511a3412f835f18294560c46036
Scott Brown <scottyb.3378@gmail.com>
2019-04-06 11:14:20 +1100

    remove unneeded code, polish

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ Uses Spotify, who may frown upon such use of their API. As such this is not for 
 
 ## TODO
 - don't use set text - replace resource text with placeholders
-- skip song, because that one Sigur Ros song is just too much
 - Notification before and during alarm for easy access, no surprises, good experience
-- Show art for song being played - either in notification like Spotify, or whole background
-- Configure a playlist in GUI rather than a hardcoded list
+- Configure a playlist in GUI rather than a hardcoded list - easy way is a textbox that a playlist id can be pasted into.
+	- Hard way is to use https://api.spotify.com/v1/search to search for playlists, display them, and allow the user to choose one
+	- this needs auth, which would be taken from the old songplayingactivity code - it was successfully doing auth, it just couldn't do it while the phone was locked. Not an issue here.
+	- also ideally it would display the album art for each playlist, or some identifying factor to differentiate those that share the same name
 
 ## Accomplished TODOs
+- Show art for song being played - either in notification like Spotify, or whole background - i get this for free by yielding control to the spotify app
+- skip song, because that one Sigur Ros song is just too much - i get this for free by yielding control to the spotify app
 - Properly format time of currently set alarm
 - Improve UI for text and buttons
 - more tests
--- app/src/main/java/com/naur/unsleepify/SongPlayingActivity.java
+++ app/src/main/java/com/naur/unsleepify/SongPlayingActivity.java
@@ -1,9 +1,6 @@
 package com.naur.unsleepify;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.spotify.android.appremote.api.ConnectionParams;
@@ -11,7 +8,6 @@ import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
 
 public class SongPlayingActivity extends Activity {
     public static final String CLIENT_ID = "6e71d381582a43f2aa3c0366bbe48ea3";
@@ -19,7 +15,7 @@ public class SongPlayingActivity extends Activity {
     private TextView view;
     private SpotifyAppRemote spotifyAppRemote;
 
-    // todo these will be removed once playlist is made configurable
+    // todo this will be removed once playlist is made configurable
     private String playlistId = "3pBnQakqa3Cd13p4qQP5Rn";
 
     @Override
@@ -36,15 +32,12 @@ public class SongPlayingActivity extends Activity {
 
         SpotifyAppRemote.connect(this, connectionParams,
                 new Connector.ConnectionListener() {
-
                     public void onConnected(SpotifyAppRemote spotifyAppRemoteParam) {
                         spotifyAppRemote = spotifyAppRemoteParam;
                         connected();
-
                     }
 
-                    public void onFailure(Throwable throwable) {
-                    }
+                    public void onFailure(Throwable throwable) {}
                 });
     }
 
@@ -54,96 +47,22 @@ public class SongPlayingActivity extends Activity {
         spotifyAppRemote.getPlayerApi().play("spotify:playlist:" + playlistId);
 
         startActivity(getPackageManager().getLaunchIntentForPackage("com.spotify.music"));
-        // launching the app is better than showing my own, so delete that code.
         spotifyAppRemote.getPlayerApi()
                 .subscribeToPlayerState()
                 .setEventCallback(new Subscription.EventCallback<PlayerState>() {
                     @Override
                     public void onEvent(PlayerState playerState) {
-//                        final Track track = playerState.track;
-//                        if (track != null) {
-//                            Log.d("MainActivity", "scott" + track.name + " by " + track.artist.name);
-//                        }
-//                        playerState.
-                        if(!playerState.isPaused){
+                        if (!playerState.isPaused) {
                             spotifyAppRemote.getPlayerApi().setShuffle(false);
 
                         }
-                        System.out.println("scott theevent"+playerState.isPaused+playerState
-                                +playerState.track.name);
-//                        spotifyAppRemote.getPlayerApi().setShuffle(false);
                     }
                 });
     }
-
 
     @Override
     protected void onDestroy() {
         SpotifyAppRemote.disconnect(spotifyAppRemote);
         super.onDestroy();
     }
-
-
-//    private void updateTextToSongInformation() {
-//        if (musicPlayer.getMetadata().currentTrack != null) {
-//            view.setText(musicPlayer.getMetadata().currentTrack.artistName + " - " + musicPlayer.getMetadata().currentTrack.name);
-//        }
-//    }
-
-//    private void playPlaylistInOrder(final Player musicPlayer, int index) {
-//        musicPlayer.playUri(null, "spotify:playlist:"+playlistId, index, 0);
-//    }
-//
-//    private void playRandomSongFromPlaylist(final Player musicPlayer) {
-//        new AsyncTask<Player, Void, Void>() {
-//            @Override
-//            protected Void doInBackground(Player... strings) {
-//
-//                List<PlaylistTrack> playlistTracks = spotifyApi.getService().getPlaylistTracks(playlistUserId, playlistId).items;
-//                int randomTrackIndex = new Random().nextInt(playlistTracks.size());
-//                playPlaylistInOrder(musicPlayer, randomTrackIndex);
-//                return null;
-//            }
-//        }.execute(musicPlayer);
-//    }
-
-//    @Override
-//    public void onLoggedIn() {
-//        musicPlayer.setShuffle(null, true);
-//        playRandomSongFromPlaylist(musicPlayer);
-//    }
-//
-//    @Override
-//    public void onPlaybackEvent(PlayerEvent playerEvent) {
-//        if (PlayerEvent.kSpPlaybackNotifyTrackChanged.equals(playerEvent)) {
-//            updateTextToSongInformation();
-//        }
-//
-//    }
-//
-//    @Override
-//    public void onPlaybackError(Error error) {
-//        if (error == Error.kSpErrorFailed) {
-//            Utils.toastify("Error playing, perhaps we got to the end of the playlist. Trying another track.", this);
-//        } else {
-//            Utils.toastify("Error playing, retrying. Details: "+error.name()+": "+error.toString(), this);
-//        }
-//        playRandomSongFromPlaylist(musicPlayer);
-//    }
-//
-//    @Override
-//    public void onLoggedOut() {
-//    }
-//
-//    @Override
-//    public void onLoginFailed(Error var1) {
-//    }
-//
-//    @Override
-//    public void onTemporaryError() {
-//    }
-//
-//    @Override
-//    public void onConnectionMessage(String message) {
-//    }
 }
