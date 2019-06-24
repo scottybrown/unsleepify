88afe85ed989308d3364a4b3acedec5a085c54f2
Scott Brown <scottyb.3378@gmail.com>
2019-06-24 21:09:42 +1000

    always play on local device

--- app/src/main/java/com/naur/unsleepify/SongPlayingActivity.java
+++ app/src/main/java/com/naur/unsleepify/SongPlayingActivity.java
@@ -1,12 +1,15 @@
 package com.naur.unsleepify;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.ConnectApi;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.CallResult;
 import com.spotify.protocol.client.Subscription;
@@ -26,33 +29,26 @@ public class SongPlayingActivity extends Activity {
         Intent startIntent = new Intent(this, MainActivity.class);
         startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(startIntent);
-        System.out.println("scott" + "onresume");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_playing);
-        System.out.println("scott" + "oncreate");
 
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
+        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID).setRedirectUri(REDIRECT_URI).showAuthView(true).build();
 
-        SpotifyAppRemote.connect(this, connectionParams,
-                new Connector.ConnectionListener() {
-                    public void onConnected(SpotifyAppRemote spotifyAppRemoteParam) {
-                        spotifyAppRemote = spotifyAppRemoteParam;
-                        connected();
-                    }
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
+            public void onConnected(SpotifyAppRemote spotifyAppRemoteParam) {
+                spotifyAppRemote = spotifyAppRemoteParam;
+                spotifyAppRemote.getConnectApi().connectSwitchToLocalDevice();
+                connected();
+            }
 
-                    public void onFailure(Throwable throwable) {
-                    }
-                });
+            public void onFailure(Throwable throwable) {
+            }
+        });
     }
-
 
     private void connected() {
         // otherwise, below when we switch off shuffle, if music is already playing it will do it too early - before the song has actually been chosen. Hence we'll get the first song in the playlist.
